9ae24360764dfc33429ac72fb4aab9da7f3e0147
gorfav <ID017437@TMLO-701607.tm.nl.ad.tmo>
2015-12-02 08:09:03 +0100

    replica

--- files/getEC.py
+++ files/getEC.py
@@ -8,23 +8,41 @@ def main():
 
 #    print 'Number of arguments:', len(sys.argv), 'arguments.'
     numArgs=len(sys.argv)
-
+    filename = "/home/ansible/staging/files/test.txt"
+    target = open(filename, 'w')
+    target.write('Test')
     if numArgs == 1:
         sys.exit()
 
     inputParam=sys.argv[1]
+    status=sys.argv[2]
 
+    target.write(inputParam+'\n')
+    target.write(status)
     process=Popen(['/usr/bin/aws','cloudformation','describe-stack-resources','--stack-name',inputParam],stdout=PIPE, stderr=PIPE)
     stdout, stderr = process.communicate()
-
     jsonObject=json.loads(stdout)
-
+    target.close()
 
     for key in jsonObject['StackResources']:
         instType=key['ResourceType']
         if instType == 'AWS::EC2::Instance':
            outstr=key['LogicalResourceId'] + " - "+ key['PhysicalResourceId']
-           print outstr
+           process=Popen(['/usr/bin/aws','ec2','describe-instance-status','--instance-id',key['PhysicalResourceId']],stdout=PIPE, stderr=PIPE)
+           stdout, stderr = process.communicate()
+           jsonObjectEC2=json.loads(stdout)
+           lenDict=len(jsonObjectEC2['InstanceStatuses'])
+           if lenDict == 0  and status == 'All':
+            print outstr
+           elif lenDict == 1:
+            for ec2Inst in jsonObjectEC2['InstanceStatuses']:
+             ec2Status=ec2Inst['InstanceState']['Name']
+             if status == 'All':
+                print outstr
+             elif (status == 'Running') and (ec2Status == 'running'):
+                print  outstr
+           if (status == 'Stopped') and (lenDict == 0):
+                print outstr
 
 if __name__ == '__main__':
     main()
